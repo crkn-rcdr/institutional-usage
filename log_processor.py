609d804175b662c9a44bc06705dc57db82df08ae
belasouza <55512909+belasouza@users.noreply.github.com>
2024-07-17 10:54:20 -0400

    Update log_processor.py
    - Update filter_logs() function
        - Name all columns from  original log file

--- log_processor.py
+++ log_processor.py
@@ -1,29 +1,31 @@
 import pandas as pd
 from ip_parser import process_ip_file, is_ip_match
 
-def filter_logs(df, reqs_ptrn, server_name):
+def filter_logs(df):
     """
-    Filters out rows in logs DataFrame based on requests and servers.   
-    
+    Filters out rows in logs DataFrame based on requests.
+
     Parameters:
     df (pd.DataFrame): Input DataFrame to be filtered.
-    reqs_ptrn (str): Request pattern to filter for.
-    server_name (str): Server name to filter for.
-    
+
     Returns:
     pd.DataFrame: Filtered DataFrame with matching rows.
     """
-    # Drop rows with NaN values in 'Request' column, if any
-    df.dropna(subset=['Request'], inplace=True)
-    
-    # Filter requests
-    df = df[df['Request'].str.contains(reqs_ptrn)]
-    
-    # Filter server
-    df = df[df['Server'] == server_name]
+    server_name = 'cap/local'
+    reqs_path = '{https|www.canadiana.ca}'
+    http_req_ptrn = 'www.canadiana.ca/view/'
+
+    # Drop rows with NaN values in 'http_request' column, if any
+    df.dropna(subset=['http_request'], inplace=True)
+
+    # Filter by server_name, request_path and http_request
+    df = df[(df['server_name'] == server_name) 
+            & ((df['request_path'].str.contains(reqs_path)) 
+               | (df['http_request'].str.contains(http_req_ptrn)))
+            & (df['http_request'].str.contains("view/"))]
     
     return df
-
+    
 def logs_to_df(file_path):
     """
     Converts a log file to a pandas DataFrame.
@@ -33,21 +35,25 @@ def logs_to_df(file_path):
 
     Returns:
     pd.DataFrame: A pandas DataFrame containing the data from the file.
-                  It includes columns with date, IP address, port, server and request information.
+                  It includes columns with date, IP address, server and HTTP request information.
     """
-    # Read file 
-    log = pd.read_csv(file_path,sep=" ")
-    column_names = ['Month', 'Day', 'Time', 'IP Address:Port', 'Server', 'Request']
-
-    # Select the relevant columns, create a new DataFrame and rename columns
-    log_df = log[[log.columns[0], log.columns[1], log.columns[2], log.columns[5], log.columns[8], log.columns[18]]].copy()
-    log_df.columns = column_names
-
-    # Split IP Address and Port into two separate columns
-    log_df[['IP Address', 'Port']] = log_df['IP Address:Port'].str.split(':', expand=True)
+    # Define column names
+    column_names = ["month", "day", "time", "hostname", "process_id", "client_ip_port", "accept_date", "frontend_name",
+                    "server_name", "timers", "http_status_code", "bytes_read", "captured_request_cookie",
+                    "captured_response_cookie", "termination_state", "retries", "connections_counts", "request_path", "http_request"]
     
-    # Drop the 'IP Address:Port' column
-    log_df.drop(columns=['IP Address:Port'], inplace=True)
+    # Read log file
+    log_df = pd.read_csv(file_path, sep=" ", header=None, names=column_names, index_col=False)
+
+    # Split 'client_ip_port' to 'client_ip' and 'client_port'
+    log_df[['client_ip', 'client_port']] = log_df['client_ip_port'].str.split(':', expand=True)
+
+    # Drop the original combined column after splitting
+    log_df = log_df.drop(columns=['client_ip_port'])
+
+    # Select only relevant columns for analysis
+    selected_columns = ["month", "day", "time", "client_ip", "server_name", "request_path", "http_request"]
+    log_df = log_df[selected_columns].copy()
 
     return log_df
 
@@ -57,9 +63,6 @@ def main():
     # Specify the number of rows to skip at the beginning of the Excel file (e.g., header rows)
     skip_rows = 2
     ips_df = process_ip_file(ip_file, skip_rows)
-    print(ips_df)
-    # Uncomment to export institutions to csv
-    #ips_df.to_csv('data/insts_df.csv', index=False)
 
     # Convert log file to pandas DataFrame
     #log_file = 'data/haproxy-traffic.log'
@@ -67,9 +70,7 @@ def main():
     log_df = logs_to_df(log_file)
 
     # Filter log_df 
-    reqs_ptrn = 'www.canadiana.ca/view/'
-    server_name = 'cap/local'
-    filtered_log_df = filter_logs(log_df, reqs_ptrn=reqs_ptrn, server_name=server_name)
+    filtered_log_df = filter_logs(log_df)
     print(filtered_log_df.head())
     filtered_log_df.to_csv('data/filtered_logs.csv', index=False)
 
@@ -78,9 +79,7 @@ def main():
 
     # Iterate through each institution and check accesses
     for institution_name, ip_networks in zip(ips_df['Institution'], ips_df['IPs']):
-        #print(f'Institution: {institution_name}')
-        mask = filtered_log_df['IP Address'].apply(lambda x: is_ip_match(x, ip_networks))
-        #print(f'Access Count: {mask.sum()}')
+        mask = filtered_log_df['client_ip'].apply(lambda x: is_ip_match(x, ip_networks))
         inst_view_counts[institution_name] = mask.sum()
 
     # Output view counts
@@ -88,7 +87,9 @@ def main():
     # for institution, count in inst_view_counts.items():
     #     print(f"Institution: {institution}, Access Count: {count}")
     
-    # inst_views_df = pd.DataFrame(list(inst_view_counts.items()), columns=['Institution', 'Views'], index=None)
+    # Convert dictionary to Data Frame
+    inst_views_df = pd.DataFrame(list(inst_view_counts.items()), columns=['Institution', 'Views'])
+    # UNcomment to xport view count to csv
     # inst_views_df.to_csv("data/result.csv", index=None)
 
 
