1cada3b48165e723635ab23342f44223df408f4d
belasouza <55512909+belasouza@users.noreply.github.com>
2024-08-02 12:15:12 -0400

    Update log_processor.py
    - Add filter per client_ip in filter_logs() function.
    - Moved report creation to usage_report.py

--- log_processor.py
+++ log_processor.py
@@ -1,12 +1,13 @@
 import pandas as pd
-from ip_parser import process_ip_file, is_ip_match
-from dotenv import load_dotenv
-import os
+from ip_parser import is_ip_match
+#from dotenv import load_dotenv
+#import os
+import re
 
-def filter_logs(df):
+def filter_logs(df, reqs_paths, http_req_ptrn, ip_networks):
     """
     Filters out rows in logs DataFrame based on requests.
-
+ 
     Parameters:
     df (pd.DataFrame): Input DataFrame to be filtered.
 
@@ -14,20 +15,21 @@ def filter_logs(df):
     pd.DataFrame: Filtered DataFrame with matching rows.
     """
     # Load environment variables from .env file
-    load_dotenv()
+    #load_dotenv()
 
-    server_name = os.getenv('SERVER_NAME')
-    reqs_path = '{https|www.canadiana.ca}'
-    http_req_ptrn = 'www.canadiana.ca/view/'
+    #server_name = os.getenv('SERVER_NAME')
+
+    # Join the items in reqs_path with a pipe symbol
+    reqs_paths = '|'.join(re.escape(item) for item in reqs_paths)
 
     # Drop rows with NaN values in 'http_request' column, if any
     df.dropna(subset=['http_request'], inplace=True)
 
-    # Filter by server_name, request_path and http_request
-    df = df[(df['server_name'] == server_name) 
-            & ((df['request_path'].str.contains(reqs_path)) 
+    # Filter by request_path, http_request and ip
+    df = df[((df['request_path'].str.contains(reqs_paths)) 
                | (df['http_request'].str.contains(http_req_ptrn)))
-            & (df['http_request'].str.contains("view/"))]
+            & (df['http_request'].str.contains("view/"))
+            & (df.apply(lambda row: is_ip_match(row['client_ip'], ip_networks), axis=1))]
     
     return df
     
@@ -61,104 +63,3 @@ def logs_to_df(file_path):
     log_df = log_df[selected_columns].copy()
 
     return log_df
-
-def process_logs(log_file):
-    """
-    Processes log data to count the number of accesses for each institution based on IP addresses.
-
-    This function reads IP addresses from an Excel file and log data from a specified log file, 
-    filters the logs, and counts the number of accesses for each institution based on the IP addresses 
-    associated with them. The results are returned as a DataFrame with the institution names and 
-    their corresponding view counts.
-
-    Args:
-        log_file (str): The path to the log file to be processed.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing two columns:
-                      - `institution`: The name of the institution.
-                      - `view_count`: The number of times the institution was accessed.
-    """
-    # Convert IP addresses file to pandas DataFrame
-    ip_file = 'data/IP_addresses.xlsx'
-    # Specify the number of rows to skip at the beginning of the Excel file (e.g., header rows)
-    skip_rows = 2
-    ips_df = process_ip_file(ip_file, skip_rows)
-
-    log_df = logs_to_df(log_file)
-
-    # Filter log_df 
-    filtered_log_df = filter_logs(log_df)
-
-    # Count accesses for each institution
-    view_counts = {}
-
-    # Iterate through each institution and check accesses
-    for institution_name, ip_networks in zip(ips_df['Institution'], ips_df['IPs']):
-        mask = filtered_log_df['client_ip'].apply(lambda x: is_ip_match(x, ip_networks))
-        view_counts[institution_name] = mask.sum()
-
-    # Convert dictionary to Data Frame
-    view_counts_df = pd.DataFrame(list(view_counts.items()), columns=['institution', 'view_count'])
-
-    return view_counts_df
-
-def count_views(log_file, ips_df, inst_name):
-    """
-    Counts the number of views for a specific institution based on IP logs.
-
-    This function reads an Excel file containing IP addresses associated with institutions, 
-    filters log data based on IP addresses, and counts the number of accesses for the 
-    specified institution.
-
-    Parameters:
-    log_file (str): Path to the log file containing IP address access records.
-    inst_name (str): The name of the institution to count views for.
-
-    Returns:
-    pd.DataFrame: A DataFrame with two columns: `institution` and `views_count`. 
-                  It contains the institution name and the total number of views 
-                  recorded for that institution.
-    """
-    # Get row for inst_name
-    # add case that institution not found ask again or provide options idk
-    inst_ips = ips_df[ips_df['Institution'] == inst_name]
-    
-    # Extract the IP networks for the institution
-    ip_networks = inst_ips.iloc[0]['IPs']
-
-    # Access log file
-    log_df = logs_to_df(log_file)
-
-    # Filter log_df 
-    filtered_log_df = filter_logs(log_df)
-
-    # Count accesses for the specific institution
-    mask = filtered_log_df['client_ip'].apply(lambda x: is_ip_match(x, ip_networks))
-    views_count = mask.sum()
-
-    # Create DataFrame for the view counts
-    view_counts_df = pd.DataFrame({'institution': [inst_name], 'views_count': [views_count]})
-
-    return view_counts_df
-
-
-def main():
-    log_file = 'data/haproxy-traffic.log'
-    ip_file = 'data/IP_addresses.xlsx'
-    skip_rows = 2
-    inst_name = 'Canadian Research Knowledge Network'
-
-    # Load institutions from the IP addresses file
-    ips_df = process_ip_file(ip_file, skip_rows)
-    
-    # Count the number of views for inst_name
-    view_counts_df = count_views(log_file, ips_df, inst_name)
-
-    # Uncomment to export view count to csv
-    # view_counts_df.to_csv("data/result.csv", index=None)
-    
-    print(view_counts_df)
-
-if __name__ == '__main__':
-    main()
