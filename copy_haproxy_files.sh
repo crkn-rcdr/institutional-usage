f7d3985539056555623d0a0c7337f5a97c1a7b90
abhinavcrkn <142815885+abhinavcrkn@users.noreply.github.com>
2024-08-02 22:27:35 +0100

    Create copy_haproxy_files.sh

---
+++ copy_haproxy_files.sh
@@ -0,0 +1,20 @@
+# This script copies log files from one platform server to my personal VM. Currently the smbclient utility is not installed on the platform servers so I'm copying the files
+# over to my VM before transferring them to the usagereport share on teran.
+
+bin/bash
+
+# Define variables
+REMOTE_USER="abhinavk"
+REMOTE_HOST="arinto.uab.c7a.ca"
+REMOTE_PATH="/var/log/"
+LOCAL_PATH="/home/abhinavk/Logs/Arinto/"
+
+# Use scp to copy files
+scp "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}haproxy-traffic*" "$LOCAL_PATH"
+
+# Verify the operation
+if [ $? -eq 0 ]; then
+  echo "Files copied successfully."
+else
+  echo "Error occurred during file transfer."
+fi
