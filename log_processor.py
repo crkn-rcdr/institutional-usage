2cd0d95b3b3571f62baec2e5c90c8f54f05a8651
Isabela Souza Cefrin <isabelacefrin@gmail.com>
2024-08-08 12:14:56 -0300

    Update log_processor.py
    - Troubleshooting logs_to_df()
    - Filtering by server in filter_logs()
    - Adding filter_ips() function

--- log_processor.py
+++ log_processor.py
@@ -1,10 +1,25 @@
 import pandas as pd
 from ip_parser import is_ip_match
-#from dotenv import load_dotenv
-#import os
+from dotenv import load_dotenv
+import os
 import re
 
-def filter_logs(df, reqs_paths, http_req_ptrn, ip_networks):
+def filter_ips(df, ip_networks):
+    """
+    Filters out rows in logs DataFrame based on ip address.
+   
+    Parameters:
+    df (pd.DataFrame): Input DataFrame to be filtered.
+    ip_networks (list): List of ips to filter for.
+    
+    Returns:
+    pd.DataFrame: Filtered DataFrame with matching rows.
+    """
+    df = df[df.apply(lambda row: is_ip_match(row['client_ip'], ip_networks), axis=1)] 
+    
+    return df
+    
+def filter_logs(df, reqs_paths, http_req_ptrn):
     """
     Filters out rows in logs DataFrame based on requests.
  
@@ -15,9 +30,9 @@ def filter_logs(df, reqs_paths, http_req_ptrn, ip_networks):
     pd.DataFrame: Filtered DataFrame with matching rows.
     """
     # Load environment variables from .env file
-    #load_dotenv()
+    load_dotenv()
 
-    #server_name = os.getenv('SERVER_NAME')
+    server_name = os.getenv('SERVER_NAME')
 
     # Join the items in reqs_path with a pipe symbol
     reqs_paths = '|'.join(re.escape(item) for item in reqs_paths)
@@ -26,10 +41,10 @@ def filter_logs(df, reqs_paths, http_req_ptrn, ip_networks):
     df.dropna(subset=['http_request'], inplace=True)
 
     # Filter by request_path, http_request and ip
-    df = df[((df['request_path'].str.contains(reqs_paths)) 
+    df = df[(df['server_name'] == server_name)
+            & ((df['request_path'].str.contains(reqs_paths)) 
                | (df['http_request'].str.contains(http_req_ptrn)))
-            & (df['http_request'].str.contains("view/"))
-            & (df.apply(lambda row: is_ip_match(row['client_ip'], ip_networks), axis=1))]
+            & (df['http_request'].str.contains("view/"))]
     
     return df
     
@@ -50,7 +65,7 @@ def logs_to_df(file_path):
                     "captured_response_cookie", "termination_state", "retries", "connections_counts", "request_path", "http_request"]
     
     # Read log file
-    log_df = pd.read_csv(file_path, sep=" ", header=None, names=column_names, index_col=False)
+    log_df = pd.read_csv(file_path, sep=r'\s+', header=None, names=column_names, index_col=False, on_bad_lines='warn') 
 
     # Split 'client_ip_port' to 'client_ip' and 'client_port'
     log_df[['client_ip', 'client_port']] = log_df['client_ip_port'].str.split(':', expand=True)
