adec44aebbb2ff20c58e95794b6a97487e6cfc04
belasouza <55512909+belasouza@users.noreply.github.com>
2024-12-13 12:01:42 -0500

    Update run_usage_reports.sh
    - Fix python command line

--- run_usage_reports.sh
+++ run_usage_reports.sh
@@ -1,6 +1,5 @@
-# This script automates the process of generating reports for a list of institutions using a specified log file.
-
 #!/bin/bash
+# This script automates the process of generating reports for a list of institutions using a specified log file.
 
 # Check if the log file argument is provided
 if [ "$#" -ne 1 ]; then
@@ -21,5 +20,5 @@ IFS=$'\n' read -d '' -r -a INSTITUTIONS < "$file"
 # Loop through each institution and run the Python script with the log file
 for institution in "${INSTITUTIONS[@]}"; do
     echo "Running report for: $institution with log file: $LOG_FILE"
-    python3 "$PYTHON_SCRIPT" "$institution" "$LOG_FILE"
+    python "$PYTHON_SCRIPT" "$institution" "$LOG_FILE"
 done
